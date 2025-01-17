9ebe910fa27f1b229f6e948d71ae5fcaaab98145
D0NI1 <154246036+D0NI1@users.noreply.github.com>
2025-01-17 22:26:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function Home(){
+    window.location.href = 'home.html';
+}
+
+function About(){
+    window.location.href = 'about.html';
+}
+
+function Order(){
+    window.location.href = 'order.html';
+}
+
+function Contact(){
+    window.location.href = 'contact.html';
+}
